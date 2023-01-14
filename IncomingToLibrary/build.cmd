fb7c88f1ff83548f9a2033463054f68c4b0a5365
Murray Grant <github@ligos.net>
2023-01-14 15:55:48 +1100

    Update build for net6.0

--- IncomingToLibrary/build.cmd
+++ IncomingToLibrary/build.cmd
@@ -1,10 +1,10 @@
 rem Clean before building
 dotnet clean -c Release
 
-rem Publish for .NET Core 2.1
-dotnet publish -c Release -f netcoreapp2.1 
+rem Publish for .NET
+dotnet publish -c Release -f net6.0
 
 rem Delete config files so we can deploy
-del /q bin\Release\netcoreapp2.1\publish\appsettings*.json
+del /q bin\Release\net6.0\publish\appsettings*.json
 
 pause
