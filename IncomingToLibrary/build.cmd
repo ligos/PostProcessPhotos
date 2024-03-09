4e5fad0f6d1d8c6d24e4585eccf52fe4907d3c2c
Murray Grant <github@ligos.net>
2024-03-09 17:37:57 +1100

    Bump deployment to target net80

--- IncomingToLibrary/build.cmd
+++ IncomingToLibrary/build.cmd
@@ -2,9 +2,9 @@ rem Clean before building
 dotnet clean -c Release
 
 rem Publish for .NET
-dotnet publish -c Release -f net6.0
+dotnet publish -c Release -f net8.0
 
 rem Delete config files so we can deploy
-del /q bin\Release\net6.0\publish\appsettings*.json
+del /q bin\Release\net8.0\publish\appsettings*.json
 
 pause
